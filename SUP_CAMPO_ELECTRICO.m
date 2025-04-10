092ca80ec57c8123b08464028fd65ccc8e6bab53
urnice1111 <emilianogarram2910@gmail.com>
2025-04-09 20:07:03 -0600

    Capa de arriba puesta / Limite abajo de semicirculo puesto

--- SUP_CAMPO_ELECTRICO.m
+++ SUP_CAMPO_ELECTRICO.m
@@ -1,28 +1,35 @@
-%%
 clc; clear; close all;
-u=linspace(0,2*pi,15);
+u=linspace(0,pi/2,15);
 v=linspace(0,2*pi,15);
 
 a=4;
+t=linspace(-a,a,15); %parametrizacion para el plano, con medidas del radio del semicirculo
 
 [U,V]=meshgrid(u,v);
 
-X=a*sin(U).*cos(V);
-Y=a*sin(U).*sin(V);
+X=a*sin(U).*sin(V);
+Y=a*sin(U).*cos(V);
 Z=a*cos(U);
 
-xt=linspace(-5,5,6);
-yt=linspace(-5,5,6);
-zt=linspace(-5,5,6);
+[X_p, Y_p] = meshgrid(t, t);               
+Z_p = ones(size(X_p)) * (max(Z(:)) + 2); %plano colocado a la altura maxima del circulo +5 
 
+
+xt=linspace(-5,5,6);
+yt=xt;
+zt=xt;
 [Ox,Oy,Oz]=meshgrid(xt,yt,zt);
+
 
 
 
 
 [rows, cols] = size(X);
 
-q1=-10;
+
+
+q1=10;
+q2=-10;
 
 Ex_total=0;
 Ey_total=0;
@@ -42,10 +49,34 @@ for i = 1:rows
     end
 end
 
+for i = 1:rows
+    for j = 1:cols
+        x = X_p(i, j);
+        y = Y_p(i, j);
+        z = Z_p(i, j);
+        [Exf,Eyf,Ezf]=coulomb(q2,Ox,Oy,Oz,x,y,z);
+
+        Ex_total=Ex_total+Exf
+        Ey_total=Ey_total+Eyf;
+        Ez_total=Ez_total+Ezf;
+    end
+end
+
 [Xn,Yn,Zn]=norm_coulomb(Ex_total,Ey_total,Ez_total);
 
 
+
+%limite de abajo circulo
+mask = Oz >= min(Z(:)); 
+
+Xn_filtered = Xn .* mask;
+Yn_filtered = Yn .* mask;
+Zn_filtered = Zn .* mask;
+
+figure;
 hold on;
-q1=quiver3(Ox,Oy,Oz,Xn,Yn,Zn,"off");
-surf(X,Y,Z)
-daspect([1,1,1])
+quiver3(Ox,Oy,Oz,Xn_filtered,Yn_filtered,Zn_filtered,"off");
+surf(X,Y,Z) %surf of the semicircle
+surf(X_p,Y_p,Z_p)
+view(3)
+daspect([1 1 1])
